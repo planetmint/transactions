c08cf86a939c92e0fc7be6fb6e295e96aeeab0e8
Jürgen Eckel <juergen@riddleandcode.com>
2023-01-25 22:02:50 +0100

    added compose to_dict, from_dict && validate tests
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- tests/types/test_compose.py
+++ tests/types/test_compose.py
@@ -12,9 +12,17 @@ def test_valid_compose_single_input_same_owner(signed_create_tx, user_pub):
     inputs = signed_create_tx.to_inputs()
     assets = [signed_create_tx.id, "QmW5GVMW98D3mktSDfWHS8nX2UiCd8gP1uCiujnFX4yK8n"]
     compose_tx = Compose.generate(inputs, [([user_pub], 1)], assets)
+
     assert compose_tx
     assert len(compose_tx.outputs) == 1
     assert user_pub in compose_tx.outputs[0].public_keys
+    
+    from transactions.common.transaction import Transaction
+    from transactions.common.schema import validate_transaction_schema
+    dict = compose_tx.to_dict()
+    Transaction.from_dict(dict)
+    validate_transaction_schema(dict)
+    
 
 # Test valid compose 1 input different owner
 def test_valid_compose_single_input_different_owner(signed_create_tx, user2_pub):
